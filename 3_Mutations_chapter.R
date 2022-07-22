fe9b8c9d4e089f4fdfff3433d248caeb1fd97922
RuthCoronaMoreno <ruth.corona.m@gmail.com>
2022-07-21 20:43:43 -0500

    Bases to model mutations of cells

---
+++ 3_Mutations_chapter.R
@@ -0,0 +1,8 @@
+#Write a program to simulate just one generation of the LD experiment - stochastically. Simulate C = 500
+#cultures each of which has N = 1000 cells and  = 10????3, i.e., a very high mutation rate. What is the
+#distribution of resistant mutants that you observe across all the cultures? Are they similar or dissimilar to
+#each other? Specify your measurement of c(m), i.e., the number of cultures with m resistant mutants. Is
+#this distribution well t by a Poisson distribution? If so, what is the best t shape parameter of the Poisson
+#density function and how does that relate to the microscopic value of mutation you used to generate the
+#output? Finally, to what extent are the fluctuations \large" or \small"?
+#[Answer]
